dcd1104352f1e6a9f27c94903fa8efc0ee6663d3
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-31 18:17:15 -0400

    add second test

--- src/tests/test_boroughs.py
+++ src/tests/test_boroughs.py
@@ -3,17 +3,20 @@ from datetime import datetime
 import pytest
 
 
-def _register_borough(testapp, **kwargs):
-    return testapp.post_json(url_for('api.create_borough'), {
-        "name": 'friendbar'
-    })
-
-
 @pytest.mark.usefixtures('db')
 class TestBoroughs:
 
     def test_create_borough(self, testapp):
-        _register_borough(testapp)
+        resp = testapp.post_json(url_for('api.create_borough'), {
+            "name": 'friendbar'
+        })
+        assert resp.json['id'] == 1
+        assert resp.status_code == 201
+
+    def test_retreive_borough(self, testapp):
+        testapp.post_json(url_for('api.create_borough'), {
+            "name": 'friendbar'
+        })
         resp = testapp.get(url_for('api.get_boroughs'))
         assert resp.status_code == 200
         assert resp.json['items'][0]['name'] == 'friendbar'
